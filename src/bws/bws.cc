f52e78eea3111d802696bd55947a1a6712d06975
lpzun <peizunliu@gmail.com>
2016-06-18 23:09:39 -0400

    First Commit

---
+++ src/bws/bws.cc
@@ -0,0 +1,21 @@
+/**
+ * @brief BWS.cc
+ *
+ * @date  : Jun 18, 2016
+ * @author: lpzun
+ */
+
+#include "bws.hh"
+
+namespace ucob {
+
+BWS::BWS() {
+    // TODO Auto-generated constructor stub
+
+}
+
+BWS::~BWS() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace ucob */
---
+++ src/bws/bws.hh
@@ -0,0 +1,21 @@
+/**
+ * @brief BWS.hh
+ *
+ * @date  : Jun 18, 2016
+ * @author: lpzun
+ */
+
+#ifndef BWS_BWS_HH_
+#define BWS_BWS_HH_
+
+namespace ucob {
+
+class BWS {
+public:
+    BWS();
+    ~BWS();
+};
+
+} /* namespace ucob */
+
+#endif /* BWS_BWS_HH_ */
