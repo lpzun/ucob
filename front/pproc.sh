1755b41f037cc81c91c9bb46461bc8b1f96126d7
lpzun <lpzun@10.0.1.33>
2017-12-18 16:26:22 -0500

    a new version of shell script of pproc

--- front/pproc.sh
+++ front/pproc.sh
@@ -1,5 +1,5 @@
 ################################################################################
-#  NOTE: This is an shell script for bopp preprocessor. It aims at translating# 
+#  NOTE: This is a shell script for bopp preprocessor. It aims at translating # 
 #  SatAbs BP to Our BP: mainly renaming labels                                #
 #                                                                             #
 #  INPUT  : a SatAbs Boolean program file                                     #
@@ -8,14 +8,43 @@
 #!/bin/bash                                                                 # 
 JAVA=java
 JAR_FLAG=-jar
-PProc=BoPP.jar         # Preprocessor: SatAbs BP -> Our BP
+JAVA_EXE=BoPP.jar         # Java Preprocessor: SatAbs BP -> Our BP
+PYTHON=python 
+PYTHON_EXE=BoPP.py          # Python preprocessor
 
 ################################################################################
-# The following is how to redirect preprocessor's output to BoPP and output   # 
-# the control flow grah and WP into a file                                    #
+# The following code goes through all Boolean programs in a given directory   #
+# specified by paramter [-d | --dir]; you can also specify which translator   #
+# to use: the java version or the python version                              #
 ################################################################################
-DIR=
-for file in $(find ${DIR} -name '*.bp' | sort); do
-    $JAVA $JAR_FLAG $PProc ${file}
-done 
+DIR=          #
+IS_JAVA=0     #
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --dir )      shift
+                          DIR=$1
+                          ;;
+        -j | --java )     IS_JAVA=1
+                          ;;
+        -h | --help )     usage
+                          exit
+                          ;;
+        * )               usage
+                          exit 1
+    esac
+    shift
+done
+echo -n "Preprocessing Boolean programs in ${DIR} with "
+if [ ${IS_JAVA} = 1 ]; then
+	echo "${JAVA_EXE}"
+    for file in $(find ${DIR} -name '*.bp' | sort); do
+        $JAVA $JAR_FLAG $JAVA_EXE ${file}
+    done
+else
+	echo "${PYTHON_EXE}"
+	for file in $(find ${DIR} -name '*.bp' | sort); do
+        $PYTHON $PYTHON_EXE -f ${file} -r
+    done
+fi 
 
